8ab4943e2a61a6256a5df364f641212de043fdf6
Piyush362 <piyushsagarofficial@gmail.com>
2025-10-17 09:43:20 +0530

    add tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,5 +10,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
 })
